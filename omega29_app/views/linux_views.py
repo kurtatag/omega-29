a55583ccfe7668078065155529d08804df1b1b08
Khetag Tebiev <kurtatag@gmail.com>
2019-11-29 23:15:01 +0300

    Add 'Home/Linux/LFCS/01 Essential Commands' page

--- omega29_app/views/linux_views.py
+++ omega29_app/views/linux_views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from omega29_app.utils.web_utils import get_list
+from omega29_app.utils.web_utils import get_list, get_page
 
 bp = Blueprint('linux', __name__, url_prefix='/linux')
 
@@ -12,3 +12,8 @@ def home():
 @bp.route('/lfcs/')
 def lfcs():
     return get_list(node_name='Home/Linux/LFCS')
+
+
+@bp.route('/lfcs/01_essential_commands')
+def lfcs_01_essential_commands():
+    return get_page(node_name='Home/Linux/LFCS/01 Essential Commands')
